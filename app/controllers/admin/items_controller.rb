42673490d2d1136151d79523b4973bf13cbab199
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-01-08 06:03:22 +0000

    adminのコントローラー作成

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,19 @@
+class Admin::ItemsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def crete
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  #namespace :admin do
-    #root to:'homes#top'
-  #end
+  namespace :admin do
+    root to:'homes#top'
+    resources :orders, only: [:show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :items, only: [:index, :create, :new, :show, :edit, :update]
+    resources :order_details, only: [:update]
+  end
 
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
