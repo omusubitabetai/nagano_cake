78a146b469377370edd49dd400ad77be88faf9b7
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-01-09 10:48:27 +0000

    管理者側商品ページ関連の洗い直し

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,44 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all.page(params[:page]).per(10)
   end
 
   def new
     @item = Item.new
   end
 
-  def crete
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash.now[:success] = "商品の新規登録が完了しました。"
+      redirect_to admin_item_path(@item)
+    else
+      flash.now[:danger] = "商品の新規登録内容に不備があります。"
+      render :new
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
-  
+
   def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+       flash.now[:success] = "商品詳細の変更が完了しました。"
+       redirect_to admin_item_path(@item)
+     else
+       flash.now[:danger] = "商品詳細の変更内容に不備があります。"
+       render :edit
+     end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
