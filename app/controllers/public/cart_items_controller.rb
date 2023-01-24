3763e2f939dc65b8b6e975e5f7cb7f776ff9c3fe
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-01-24 07:31:59 +0000

    publicのカート機能実装とレイアウト

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,8 +5,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new
+    @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
+    @cart_item.save
     redirect_to public_cart_items_path
   end
 
@@ -28,4 +29,8 @@ class Public::CartItemsController < ApplicationController
 		redirect_to public_cart_items_path
   end
 
+  private
+    def cart_item_params
+      params.require(:cart_item).permit(:item_id,:amount)
+    end
 end
