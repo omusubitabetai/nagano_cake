34ddcd7cc28e7e0e7c981944ae4d2fd69fbae663
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-01-29 14:01:01 +0000

    提出前見直し３

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -19,7 +19,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    public_customers_path(resource)
+    root_path(resource)
   end
 
   def after_sign_out_path_for(resource)
