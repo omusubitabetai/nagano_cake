554f8a67413b87ceb4bf6d3c6b9fe0846fa7b142
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-01-30 12:06:29 +0000

    提出前見直し4

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
 	belongs_to :item
+	
+	
+	def subtotal
+	 item.price*1.1.floor*cart_item.amount
+	end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
    has_many :cart_items
    has_many :order_items
    has_one_attached :image
+
+   def tax_in_price
+     (price*1.1).floor
+   end
 end
